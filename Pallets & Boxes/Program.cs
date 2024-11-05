0999ef4040da95cda9b349633afa30473464e219
MetallicSky <metlaski.corporation@gmail.com>
2024-11-05 07:30:10 +0300

    Add project files.

---
+++ Pallets & Boxes/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string logo =
+        "       ╔═════════╗╔═══╗\n" +
+        "       ║ Pallets ║║ & ║\n" +
+        "       ╚═════════╝╚═══╝\n" +
+        "    ╔═══════════════════╗\n" +
+        "    ║       Boxes       ║\n" +
+        "    ╚═══════════════════╝\n" +
+        "   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n\n";
+        Console.WriteLine(logo);
+        Console.WriteLine("Number of arguments: " + args.Length);
+    }
+}
